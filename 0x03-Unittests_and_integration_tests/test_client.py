e35f3a59b89e11ac0c6ed000d2505463568a2d92
AyanSwarga <ayannicholas@gmail.com>
2023-10-27 15:34:22 +0100

    file

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""Test Class for Gitbub client"""
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Github Org Client"""
+
+    def test_org(self):
+        """test_org method"""
+        pass
